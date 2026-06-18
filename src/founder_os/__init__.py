e6d6ff8e8928e7a96e5d87c343c9897f69ddc7b7
Aditya <aditya89bh@gmail.com>
2026-06-18 11:41:38 +0530

    Initialize package structure

---
+++ src/founder_os/__init__.py
@@ -0,0 +1,5 @@
+"""Founder Operating System: a personal operating system for founders.
+
+This package provides the typed domain models and command-line interface that
+form the foundation of the Founder Operating System.
+"""
