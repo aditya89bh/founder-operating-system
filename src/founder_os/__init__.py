6a06a9e629ece70a0124e4183eb8a44a4e1fee3d
Aditya <aditya89bh@gmail.com>
2026-06-18 11:42:24 +0530

    Add version module

--- src/founder_os/__init__.py
+++ src/founder_os/__init__.py
@@ -3,3 +3,7 @@
 This package provides the typed domain models and command-line interface that
 form the foundation of the Founder Operating System.
 """
+
+from founder_os.version import __version__
+
+__all__ = ["__version__"]
